0ff5b2dd1d426cbb10cca4ab6fb31fe0a17662eb
Defor721 <92020095+Defor721@users.noreply.github.com>
2025-03-25 18:34:03 +0900

    [Feat] 협업자 추가 기능 수정

--- src/app/lib/auth.tsx
+++ src/app/lib/auth.tsx
@@ -1,8 +1,8 @@
-// app/lib/auth.ts
 import GitHubProvider from "next-auth/providers/github";
 import { AuthOptions } from "next-auth";
 import { JWT } from "next-auth/jwt";
 import { Session } from "next-auth";
+import clientPromise from "@/app/lib/mongodb"; // Mongo 연결 추가
 
 export const authOptions: AuthOptions = {
   providers: [
@@ -15,6 +15,27 @@ export const authOptions: AuthOptions = {
     async session({ session, token }: { session: Session; token: JWT }) {
       session.user.id = token.sub!;
       return session;
+    },
+
+    // ✅ 로그인 성공 시 유저를 users 컬렉션에 upsert
+    async signIn({ user }) {
+      const client = await clientPromise;
+      const db = client.db("collab-editor");
+
+      await db.collection("users").updateOne(
+        { email: user.email }, // 기준: 이메일
+        {
+          $setOnInsert: {
+            id: user.id, // next-auth의 내부 id
+            email: user.email,
+            name: user.name,
+            createdAt: new Date(),
+          },
+        },
+        { upsert: true }
+      );
+
+      return true;
     },
   },
   secret: process.env.NEXTAUTH_SECRET,
