9ed6325facfa4474d1026d2529865cd4ae78b7f9
Defor721 <92020095+Defor721@users.noreply.github.com>
2025-04-05 13:04:49 +0900

    [Feat] CSS 수정

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,6 +4,7 @@ import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function LoginPage() {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { data: session, status } = useSession();
   const router = useRouter();
 
